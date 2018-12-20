a015a0c387b74759753716b3b989c032ca7ed25e
pedromigliatti <pedro.migliatti@gmail.com>
2018-12-20 11:45:10 -0200

    Add new properties to Article, process the strings and uris

--- src/main/java/Article.java
+++ src/main/java/Article.java
@@ -1,5 +1,10 @@
+import org.apache.commons.lang3.ObjectUtils;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Article {
     public static final ArrayList<Mapping> mappings = new ArrayList<Mapping>(Arrays.asList(
@@ -10,13 +15,60 @@ public class Article {
 
             new Mapping("paperAbstract","",Type.STRING),
 
-            new Mapping("entities", "", Type.LIST),
+            new Mapping("entities", "", Type.STRING),
 
             new Mapping("s2Url", "", Type.URI),
 
             new Mapping("s2PdfUrl", "", Type.URI),
 
-            
+            new Mapping("pdfUrls", "", Type.CUSTOM),
+
+            new Mapping("authors", "http://dbpedia.org/ontology/author", Type.CUSTOM),
+
+            new Mapping("inCitations", "http://www.w3.org/1999/xhtml/vocab#cite", Type.CUSTOM),
+
+            new Mapping("outCitations", "http://www.w3.org/1999/xhtml/vocab#cite", Type.CUSTOM),
+
+            new Mapping("year", "http://purl.org/dc/terms/issued", Type.STRING),
+
+            new Mapping("venue","http://data.europa.eu/eli/ontology#published_in", Type.STRING),
+
+            new Mapping ("journalName", "http://data.europa.eu/eli/ontology#published_in", Type.STRING),
+
+            new Mapping("journalVolume", "http://swrc.ontoware.org/ontology#volume", Type.STRING),
+
+            new Mapping("journalPages", "http://swrc.ontoware.org/ontology#pages", Type.STRING),
+
+            new Mapping("sources", "", Type.STRING),
+
+            new Mapping("doi", "", Type.STRING),
+
+            new Mapping( "doiUrl", "", Type.URI),
+
+            new Mapping("pmid", "", Type.STRING)
 
     ));
+
+    public static  List<Mapping> getMappings(){
+        return mappings;
+    }
+
+    public static Map<String,ObjectUtils.Null> getMappedTags(){
+        Map<String, ObjectUtils.Null> map = new HashMap<String,ObjectUtils.Null>();
+        for (Mapping m : mappings){
+            map.put(m.getTag(),null);
+        }
+        return map;
+    }
+
+    public static List<Mapping> getMapping(String tag){
+        List<Mapping> properties = new ArrayList<Mapping>();
+        for (Mapping m : mappings){
+            if (m.getTag().equals(tag)){
+                properties.add(m);
+            }
+        }
+
+        return properties;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,102 @@
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 
-import com.hp.hpl.jena.sparql.lib.org.json.JSONArray;
-import com.hp.hpl.jena.sparql.lib.org.json.JSONException;
-import com.hp.hpl.jena.sparql.lib.org.json.JSONObject;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.graph.Triple;
 
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.ParseException;
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     static final String FILE_PATH = "/home/pedro/Documentos/semanticscholar/src/main/resources/sample-S2-records";
 
-    public static void main(String[] argv) throws JSONException, IOException, ParseException {
-        String file = readFile(FILE_PATH);
+    public static void main(String[] argv) throws IOException {
+        HashMap<String, String> article = new HashMap<String, String>();
 
-        JSONArray jarr = new JSONArray(file);
+        InputStream inputstream = new FileInputStream(FILE_PATH);
+        JsonReader reader = new JsonReader(new InputStreamReader(inputstream, "UTF-8"));
 
-        String subject, predicate, object;
+        String name = "";
 
-        for(int i = 0; i<jarr.length(); i++){
-            JSONObject obj = jarr.getJSONObject(i);
+        reader.setLenient(true);
 
+        while (true) {
+            String s = "";
 
+            JsonToken token = reader.peek();
+            switch (token) {
+                case BEGIN_ARRAY:
+                    reader.beginArray();
+                    break;
+                case END_ARRAY:
+                    reader.endArray();
+                    break;
+                case BEGIN_OBJECT:
+                    reader.beginObject();
+                    break;
+                case END_OBJECT:
+                    System.out.println(reader.getPath().split("\\.").length);
+                    if(reader.getPath().split("\\.").length<3) {
+                        processMap(article);
+                        article = new HashMap<String, String>();
+                    }
+                    reader.endObject();
+                    break;
+                case NAME:
+                    name = reader.nextName();
+                    break;
+                case STRING:
+                    s = reader.nextString();
+                    if(!name.equals(""))
+                        article.put(reader.getPath().replace("$.",""), s);
+                    break;
+                case NUMBER:
+                    s = reader.nextString();
+                    if(!name.equals(""))
+                        article.put(reader.getPath().replace("$.",""), s);
+                    break;
+                case BOOLEAN:
+                    boolean b = reader.nextBoolean();
+                    break;
+                case NULL:
+                    reader.nextNull();
+                    break;
+                case END_DOCUMENT:
+                    return;
+            }
         }
     }
 
-    public static String readFile(String filename) {
-        String result = "";
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(filename));
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-            while (line != null) {
-                sb.append(line + ",");
-                line = br.readLine();
+    private static void processMap(HashMap<String, String> article) {
+        Node subject = NodeFactory.createURI(article.get("s2Url"));
+        for(Map.Entry<String, String> entry : article.entrySet()) {
+            List<Mapping> mappings = Article.getMapping(entry.getKey());
+            for (Mapping map : mappings) {
+                Node predicate = NodeFactory.createURI(map.getPropertyUri());
+                Node object = null;
+                switch (map.getType()){
+                    case STRING:
+                        object = NodeFactory.createLiteral(entry.getValue());
+                        break;
+                    case URI:
+                        object = NodeFactory.createURI(entry.getValue());
+                        break;
+                    case CUSTOM:
+                        object = customObject(entry);
+                        break;
+                }
+                Triple t = new Triple(subject, predicate, object);
             }
-            result = sb.toString();
-        } catch(Exception e) {
-            e.printStackTrace();
         }
-        return  "[" + result + "]";
+    }
+
+    private static Node customObject(Map.Entry<String, String> map) {
+        Node object = null;
+
+        return object;
     }
 }
