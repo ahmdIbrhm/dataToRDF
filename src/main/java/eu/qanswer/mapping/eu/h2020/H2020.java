90ee090ff44893bbaebe890c49d7edc504ac5c3b
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2019-06-17 15:16:56 +0200

    H2020 mappings

---
+++ src/main/java/eu/qanswer/mapping/eu/h2020/H2020.java
@@ -0,0 +1,27 @@
+package eu.qanswer.mapping.eu.h2020;
+
+import eu.qanswer.mapping.AbstractClassMapping;
+import eu.qanswer.mapping.CSVClassMapping;
+import eu.qanswer.mapping.Mapping;
+import eu.qanswer.mapping.Type;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class H2020 extends CSVClassMapping {
+
+    public H2020(){
+        format = "csv";
+        separator = ';';
+        file="/Users/Dennis/Downloads/h2020_topics_with_budget_and_links.csv";
+        baseUrl = "http://europa.eu/";
+        key = "Topic";
+        mappings = new ArrayList<>(Arrays.asList(
+                new Mapping("Topic","http://www.w3.org/2000/01/rdf-schema#label",Type.LITERAL),
+                new Mapping("Topic Desc","http://schema.org/description", Type.LITERAL),
+                new Mapping("H2020 Signed Grants","http://www.wikidata.org/prop/direct/P800" ,Type.LITERAL),
+                new Mapping("H2020 Net EU Contribution","http://www.wikidata.org/prop/direct/P2769", Type.LITERAL),
+                new Mapping("Topic description","http://www.wikidata.org/prop/direct/P856", Type.LITERAL)
+        ));
+    }
+}
